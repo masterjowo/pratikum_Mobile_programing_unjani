52ebd352715f84df2a3f4647bf9898e68b16b794
maSTER_jowo2 Ayam <masterjowo123@gmail.com>
2022-05-17 20:52:15 +0700

    modul 5

---
+++ Prak-mobile-programming-p5/app/src/main/java/com/example/bekerja_dengan_menu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bekerja_dengan_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
